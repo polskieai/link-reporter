992fb5b3b42bfbe937f48f18ad29b5320942d386
Tomasz Klim <github@tomaszklim.pl>
2024-08-06 09:39:09 +0200

    fix for previous change (cut base directory)

--- bin/generate-domains-list.sh
+++ bin/generate-domains-list.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$2" != "" ] && [ -d "$1" ]; then
-	find $1/ -mindepth 2 -type d |egrep -v '([0-9]{1,3}\.){3}[0-9]{1,3}' |cut -d'&' -f1 >$2.new
+	find $1/ -mindepth 2 -type d |sed "s#$1/##g" |cut -d'/' -f2- |egrep -v '([0-9]{1,3}\.){3}[0-9]{1,3}' |cut -d'&' -f1 >$2.new
 	if [ -s $2.new ]; then
 		mv $2 $2.prev
 		mv $2.new $2
